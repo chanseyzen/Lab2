ceea852c05a3f94f8035091b9c50d2fa6bc05124
Zen <pzxzzzz@gmail.com>
2022-10-28 00:13:54 +0800

    this module is excellent!

--- ET07355_Lab2.py
+++ ET07355_Lab2.py
@@ -1,1 +1,18 @@
 print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python lol")
+
+def calculate_bmi(height, weight):
+    print("Height = " + str(height))
+    print("Weight = " + str(weight))
+#Add code here to calculate BMI
+    bmi = weight/(height * height)
+#Add code here to display calculate BMI
+    print("Bmi = " + str(bmi))
+    if bmi < 18.5:
+        print("UnderWeight")
+    elif bmi >= 18.5 and bmi <= 25.0 :
+        print("NormalWeight")
+    else :
+        print("OverWeight")
+
+calculate_bmi(weight=70, height=1.73)
+

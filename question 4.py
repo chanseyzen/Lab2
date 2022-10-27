d40e3c33f6c131e0629eca1eb7bf8d5545535438
Zen <pzxzzzz@gmail.com>
2022-10-28 02:08:38 +0800

    Adrian sucks

--- question 4.py
+++ question 4.py
@@ -1,13 +1,45 @@
+import statistics
+
+
 def main():
-    calc_average_temperature()
-    calc_min_max_temperature()
+    display_main_menu()
+    INPUT = get_user_input()
+    calc_average(INPUT)
+    find_min_max(INPUT)
+    sort_temperature(INPUT)
+    calc_median_temperature(INPUT)
 
-def calc_average_temperature():
-    print("Please enter your temperature :")
-    x = input()
-    print(x)
 
-def calc_min_max_temperature():
-    print("Your minimum temperature is:")
-    y = input()
-    print(y)
+def display_main_menu():
+    print("Welcome!")
+
+def get_user_input():
+    x = input("Enter numbers with commas")
+    y = x.split(",")
+    z = [float(a) for a in y]
+    print(z)
+    return z
+def calc_average(num):
+    a = sum(num)/len(num)
+    print(a)
+    return a
+def find_min_max(num):
+    MINI = min(num)
+    MAXI = max(num)
+    print(MINI,MAXI)
+    return (MINI,MAXI)
+
+def sort_temperature(num):
+    num.sort()
+    print(num)
+    return num
+
+def calc_median_temperature(num):
+    MEDIAN = statistics.median(num)
+    print(MEDIAN)
+    return MEDIAN
+
+
+main()
+
+

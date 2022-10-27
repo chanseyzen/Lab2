c520533c1d323fe85147b6bb6354de4fe6596bd1
Zen <pzxzzzz@gmail.com>
2022-10-28 00:47:24 +0800

    help me

--- ET07355_Lab2.py
+++ ET07355_Lab2.py
@@ -1,6 +1,6 @@
 print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python lol")
 
-def calculate_bmi(height, weight):
+"""def calculate_bmi(height, weight):
     print("Height = " + str(height))
     print("Weight = " + str(weight))
 #Add code here to calculate BMI
@@ -14,5 +14,22 @@ def calculate_bmi(height, weight):
     else :
         print("OverWeight")
 
-calculate_bmi(weight=70, height=1.73)
+calculate_bmi(weight=70, height=1.73)"""
+def main():
+    display_main_menu()
+    get_user_input()
+
+def display_main_menu():
+    print("Enter some numbers seperated by commas (e.g. 5,67,32):")
+
+def get_user_input():
+    x = input()
+    print("Hello," + x)
+    y = x.split()
+
+    z = [float(a) for a in y ]
+
+    print(z)
+
+main()
 
